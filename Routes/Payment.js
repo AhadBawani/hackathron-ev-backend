62f258756813e7d2d629edd50c280dab5e624ded
AhadBawani <ahadbawani123@gmail.com>
2023-04-14 08:23:15 +0530

    payment method changes

--- Routes/Payment.js
+++ Routes/Payment.js
@@ -30,8 +30,7 @@ router.post("/orders", async (req, res) => {
 	}
 });
 
-router.post("/verify", async (req, res) => {
-	const { userId, stationId, slotBooked, bookedTime, bookedDate, index, bookedDay } = req.body;
+router.post("/verify", async (req, res) => {	
 	try {
 		const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
 			req.body;
@@ -42,79 +41,7 @@ router.post("/verify", async (req, res) => {
 			.digest("hex");
 
 		if (razorpay_signature === expectedSign) {
-			// return res.status(200).json({ message: "Payment verified successfully" });
-			BookedChargingStations.findOne({ bookedDate: bookedDate, stationId: stationId, index: index })
-				.exec()
-				.then(response => {
-					if (!response) {
-						Users.findById(userId)
-							.exec()
-							.then(userResponse => {
-								if (userResponse) {
-									ChargingStationSchema.findById(stationId)
-										.exec()
-										.then(chargingStationResponse => {
-											if (chargingStationResponse) {
-												const bookedChargingStations = new BookedChargingStations({
-													userId: userId,
-													stationId: stationId,
-													slotBooked: slotBooked,
-													bookedTime: bookedTime,
-													bookedDate: bookedDate,
-													bookedDay: bookedDay,
-													index: index
-												}).save();
-
-												try {
-													bookedChargingStations
-														.then((response) => {
-															res.status(201).json({
-																message: "Charging slot booked successfully!",
-																detail: {
-																	id: response._id,
-																	userId: response.userId,
-																	stationId: response.stationId,
-																	slotBooked: response.slotBooked,
-																	bookedDate: response.bookedDate,
-																	bookedTime: response.bookedTime,
-																	bookedDay: response.bookedDay
-																}
-															})
-														})
-														.catch((error) => {
-															res.status(404).send(error);
-														})
-												}
-												catch (error) {
-													res.status(404).send(error);
-												}
-											}
-											else {
-												res.status(404).send({
-													message: "Charging Station not exists!"
-												})
-											}
-										})
-										.catch((error) => {
-											console.log(error);
-										})
-								}
-								else {
-									res.status(404).send({
-										message: "user not found!"
-									})
-								}
-							})
-					}
-					else {
-						res.status(400).send({
-							message: "Charging Slot already booked for you!"
-						})
-					}
-				})
-				.catch((error) => {
-					console.log(error);
-				})
+			return res.status(200).json({ message: "Payment verified successfully" });			
 		} else {
 			return res.status(400).json({ message: "Invalid signature sent!" });
 		}
